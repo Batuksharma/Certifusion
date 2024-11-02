e9c818661461781397f7126bd7c24eda023fa845
Batuk sharma <97529597+Batuksharma@users.noreply.github.com>
2024-11-02 22:13:44 +0530

    Update hardhat.config.js

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-YOUR_PRIVATE_KEY = 'bb259a28439ca71de344bf1f9e31da71d482abbea4333a71cb78966d41b21e8b'
+YOUR_PRIVATE_KEY = ''
 
 module.exports = {
     solidity: "0.8.27",
